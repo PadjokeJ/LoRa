1f099dc0af6ae48ae22141cf1369b6e4b75cc037
Pad <145267541+PadjokeJ@users.noreply.github.com>
2025-03-14 14:58:54 +0100

    fix encode header file

--- lib/encode.h
+++ lib/encode.h
@@ -1,13 +1,6 @@
-#ifdef __cplusplus
-extern "C"
-{
-#endif
-
 #ifndef ENCODE_H
 #define ENCODE_H
 
 uint8_t* encode(uint4_t packet_type, uint8_t source_address, uint8_t destination_address, char* message);
 
-#ifdef __cplusplus
-}
 #endif
