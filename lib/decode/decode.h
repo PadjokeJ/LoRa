ba593dc8f98013d013979e4bf6e93e0173563310
Pad <145267541+PadjokeJ@users.noreply.github.com>
2025-03-20 18:55:30 +0100

    decode test and header files

---
+++ lib/decode/decode.h
@@ -0,0 +1,8 @@
+#ifndef DECODE_H
+#define DECODE_H
+
+#include "packet.h"
+
+struct packet decode(uint8_t* packet_bytes);
+
+#endif
---
+++ lib/decode/decode_test.cpp
@@ -0,0 +1,5 @@
+#include "decode.h"
+
+int main(){
+    return 0;
+}
