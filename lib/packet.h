d6f93291b848b13d2aff0f3330355b111034f52d
Pad <145267541+PadjokeJ@users.noreply.github.com>
2025-03-20 14:16:53 +0100

    fix packet header file not having identifier

--- lib/packet.h
+++ lib/packet.h
@@ -11,5 +11,5 @@ struct packet{
     uint8_t* encoded_bytes;
 }
 
-struct packet to_packet_struct(uint8_t packet_type, uint8_t source_address, uint8_t destination_address, char* message);
+struct packet to_packet_struct(uint8_t packet_type, uint16_t identifier, uint8_t source_address, uint8_t destination_address, char* message);
 #endif
