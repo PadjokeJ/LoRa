a5e34407378bc9119df29415f1945a28e3a1bf42
Sabu8 <142678851+Sabu8c@users.noreply.github.com>
2025-03-20 13:58:26 +0100

    create forlders for functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
 #include <Arduino.h>
-#include "recieve.h"
+#include "encode/encode.h"
+#include "memory/memory.h"
+#include "process/process.h"
+#include "recieve/recieve.h"
+#include "send/send.h"
+#include "serial/serial.h"
+
 
 void setup() {
    
