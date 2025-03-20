237368f0aea4f4190556fbcee8d38816bbbe90af
Pad <145267541+PadjokeJ@users.noreply.github.com>
2025-03-20 21:29:31 +0100

    encode testing file

--- lib/encode/encode_test.cpp
+++ lib/encode/encode_test.cpp
@@ -8,9 +8,17 @@ int main(){
     uint8_t source = 0;
     uint8_t dest = 1;
 
-    char* message = "Hello World!";
+    char message[] = "Hello World!";
 
-    struct packet pack = encode(packet_type, source, dest, message);
+    struct packet pack;
+
+    encode(packet_type, source, dest, message);
+
+    printf("ID \t:\t%d\n", pack.identifier);
+    printf("Type \t:\t%d\n", pack.type);
+    printf("source \t:\t%d\n", pack.source);
+    printf("dest \t:\t%d\n", pack.destination);
+    printf("size \t:\t%d\n", pack.size);
 
     return 0;
 }
