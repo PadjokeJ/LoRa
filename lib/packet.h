24c1bc8e1099f2fe7a2982581912b9cf8ac69371
Pad <145267541+PadjokeJ@users.noreply.github.com>
2025-03-20 14:03:41 +0100

    add size to packet info

--- lib/packet.h
+++ lib/packet.h
@@ -6,6 +6,10 @@ struct packet{
     uint16_t identifier;
     uint8_t source;
     uint8_t destination;
+    uint8_t size;
     char* message;
     uint8_t* encoded_bytes;
 }
+
+struct packet to_packet_struct(uint8_t packet_type, uint8_t source_address, uint8_t destination_address, char* message);
+#endif
