5b856aa16055989a7d2b6121210a04b5cd0764a2
Pad <145267541+PadjokeJ@users.noreply.github.com>
2025-03-20 18:55:59 +0100

    finish decode script

--- lib/decode/decode.cpp
+++ lib/decode/decode.cpp
@@ -1,24 +1,34 @@
 #include "packet.h"
 
-struct packet decode(uint8_t* packet_bytes){ 
-
-
+struct packet decode(uint8_t* packet_bytes){
     struct packet decoding_packet;
+    decoding_packet.encoded_bytes = packet_bytes;
+
     uint8_t packet_type = packet_bytes[0];
-    uint8_t packet_ID = packet_bytes[1<<2];
+    decoding_packet.type = packet_type;
+    
+    uint16_t packet_ID = packet_bytes[1] <<8 + packet_bytes[2];
+    decoding_packet.identifier = packet_ID;
+
     uint8_t message_source = packet_bytes[3];
+    decoding_packet.source = message_source;
+
     uint8_t packet_dest = packet_bytes[4];
+    decoding_packet.destination = packet_dest;
+
     uint8_t message_size = packet_bytes[5];
     decoding_packet.size = message_size;
 
-    uint8_t* ptr_decoded_bytes = decoded_bytes;
+    uint8_t* ptr_packet_bytes = packet_bytes;
+    for (int i = 0; i < 6; i++)
+        ptr_packet_bytes++;
     
-    while(*ptr_message++){
-        *ptr_decoded_bytes = (char)*ptr_message;
+    
+    uint8_t* ptr_decoded_bytes{new uint8_t[message_size]};
+
+    while(*ptr_packet_bytes++){
+        *ptr_decoded_bytes = (char)*ptr_packet_bytes;
         ptr_decoded_bytes++;
     }
-
-    decoding_packet.decoded_bytes = decoded_bytes;
-
     return decoding_packet;
 }
