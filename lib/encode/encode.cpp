5d078f9ffecc0979ed3dc3f9c9ad83be50561726
PadjokeJ <jonatanr.pfister@gmail.com>
2025-03-20 15:48:00 +0100

    comment encode file + encode message info

--- lib/encode/encode.cpp
+++ lib/encode/encode.cpp
@@ -1,9 +1,10 @@
-#include "packet.h"
 #include <time.h>
 #include <stdlib.h>
 
 #include <stdint.h>
 
+
+#include "packet.h"
 
 uint8_t size_of_message(char* message){
     uint8_t size;
@@ -14,25 +15,46 @@ uint8_t size_of_message(char* message){
 }
 
 struct packet encode(uint8_t packet_type, uint8_t source_address, uint8_t destination_address, char* message){ 
-    srand(time(NULL));
-    int random_number = rand();
+    srand(time(NULL)); // set the seed of the random number generator
+    int random_number = rand(); // get a random number
 
-    uint16_t identifier = (uint16_t)random_number;
+    uint16_t identifier = (uint16_t)random_number; //convert the random number to a 16 bit type
 
-    struct packet encoding_packet = to_packet_struct(packet_type, identifier, source_address, destination_address, message);
+    struct packet encoding_packet = to_packet_struct(packet_type, identifier, source_address, destination_address, message); // convert the packet info to a struct
 
-    char* ptr_message = encoding_packet.message;
-    uint8_t message_size = size_of_message(message);
-    encoding_packet.size = message_size;
-    uint8_t encoded_bytes[message_size];
-    uint8_t* ptr_encoded_bytes = encoded_bytes;
+    char* ptr_message = encoding_packet.message; // set a pointer to the start of the message array
+
+    uint8_t message_size = size_of_message(message); // get the message size
+    encoding_packet.size = message_size; // set the message size inside the packet structure
+
+    uint8_t encoded_bytes[message_size + 6]; // create an uninitialized array with the correct size to include all the information
+    uint8_t* ptr_encoded_bytes = encoded_bytes; // set a pointer to the start of the encoded array start
     
+    *ptr_encoded_bytes = encoding_packet.type; // copy the packet type byte to the encoded bytes
+    ptr_encoded_bytes++; // move the pointer to packet id
+
+    *ptr_encoded_bytes = (uint8_t)(encoding_packet.identifier >> 8); // copy the packet identifier byte (the first eight bits) to the encoded bytes
+    ptr_encoded_bytes++; // move the pointer to packet id's second byte
+    *ptr_encoded_bytes = (uint8_t)encoding_packet.identifier; // copy the packet identifier byte (the last eight bits) to the encoded bytes
+    ptr_encoded_bytes++; // move the pointer to source
+    
+    *ptr_encoded_bytes = encoding_packet.source; // copy the packet type byte to the encoded bytes
+    ptr_encoded_bytes++; // move the pointer to dest
+
+    *ptr_encoded_bytes = encoding_packet.destination; // copy the packet type byte to the encoded bytes
+    ptr_encoded_bytes++; // move the pointer to size
+
+    *ptr_encoded_bytes = encoding_packet.size; // copy the packet type byte to the encoded bytes
+    ptr_encoded_bytes++; // move the pointer to message start
+
+
     while(*ptr_message++){
-        *ptr_encoded_bytes = (uint8_t)*ptr_message;
+        *ptr_encoded_bytes = (uint8_t)*ptr_message; // transform char to 8bit
         ptr_encoded_bytes++;
     }
 
-    encoding_packet.encoded_bytes = encoded_bytes;
+
+    encoding_packet.encoded_bytes = encoded_bytes; //set the message bytes inside of the packet structure
 
     return encoding_packet;
 }
