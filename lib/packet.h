b0a790fc02e63fcf0910b64ec38a511b0a673665
Pad <145267541+PadjokeJ@users.noreply.github.com>
2025-03-20 14:24:41 +0100

    packet struct was missing a semicolon

--- lib/packet.h
+++ lib/packet.h
@@ -11,7 +11,7 @@ struct packet{
     uint8_t size;
     char* message;
     uint8_t* encoded_bytes;
-}
+};
 
 struct packet to_packet_struct(uint8_t packet_type, uint16_t identifier, uint8_t source_address, uint8_t destination_address, char* message);
 #endif
