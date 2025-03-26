62bf1732e4352e746dd516a606fc0ff22c325d72
Maurits33 <mauritsvdnet@gmail.com>
2025-03-26 23:56:22 +0100

    Modif de Maurits

--- lib/memory/memory.cpp
+++ lib/memory/memory.cpp
@@ -1,12 +1,28 @@
 #include "memory.h"
 
-int comparaison(int id, int* tableau, int taille) {  // Déclare la fonction 'comparaison' qui prend l'ID du destinataire, l'ID du paquet, un tableau d'IDs, l'ID de l'expéditeur et la taille du tableau.
+int comparaison(int id, int* knownlist,int taille) {  // Déclare la fonction 'comparaison' qui prend l'ID du destinataire, l'ID du paquet, un tableau d'IDs, l'ID de l'expéditeur et la taille du tableau.
     int known = 0;  // Déclare et initialise la variable 'known' à 0, qui indiquera si l'ID a été trouvé dans le tableau.
-
-    for (int element_du_tableau = 0; element_du_tableau < taille; element_du_tableau++) {  // Parcourt le tableau d'IDs jusqu'à la taille actuelle du tableau.
-        if (tableau[element_du_tableau] == id) {  // Si l'élément actuel du tableau correspond à l'ID cherché.
+    int *tmp;
+    for (int i = 0; knownlist[i] >0; i++) {  // Parcourt le tableau d'IDs jusqu'à la taille actuelle du tableau.
+        if (knownlist[i] == id) {  // Si l'élément actuel du tableau correspond à l'ID cherché.
             known = 1;  // Met la variable 'tknown' à 1 si l'ID a été trouvé.
             break;  // Sort de la boucle immédiatement une fois l'ID trouvé.
         }
+        tmp=&knownlist[i];
+    }
+    tmp++;
+    if (known==0){
+        if (*tmp==-1){//empeche le debordement
+            for (int i =0;i<(taille-1);i++){
+                knownlist[i]=0;//Réinitialise la knownlist
+            knownlist[0]=id;//Ajoute l'id à la knownlist
+            }
+        printf("Liste reset\n");
+        }
+    else{
+        *tmp=id;//Ajoute l id a la knownlist
+        printf("added\n");
+    }    
     }
     return known;  // Retourne 1 si l'ID a été trouvé (known = 1), sinon retourne 0 (known = 0).
+}
--- lib/memory/memory_test.cpp
+++ lib/memory/memory_test.cpp
@@ -1,16 +1,31 @@
 #include "memory.h"
 #include <stdio.h>
 
-int comparaison(int id, int* tableau, int taille) {  // Déclare la fonction 'comparaison' qui prend l'ID du destinataire, l'ID du paquet, un tableau d'IDs, l'ID de l'expéditeur et la taille du tableau.
-    int known = 0;  // Déclare et initialise la variable 'known' à 0, qui indiquera si l'ID a été trouvé dans le tableau.
 
-    for (int element_du_tableau = 0; element_du_tableau < taille; element_du_tableau++) {  // Parcourt le tableau d'IDs jusqu'à la taille actuelle du tableau.
-        if (tableau[element_du_tableau] == id) {  // Si l'élément actuel du tableau correspond à l'ID cherché.
+int comparaison(int id, int* knownlist,int taille) {  // Déclare la fonction 'comparaison' qui prend l'ID du destinataire, l'ID du paquet, un tableau d'IDs, l'ID de l'expéditeur et la taille du tableau.
+    int known = 0;  // Déclare et initialise la variable 'known' à 0, qui indiquera si l'ID a été trouvé dans le tableau.
+    int *tmp;
+    for (int i = 0; knownlist[i] >0; i++) {  // Parcourt le tableau d'IDs jusqu'à la taille actuelle du tableau.
+        if (knownlist[i] == id) {  // Si l'élément actuel du tableau correspond à l'ID cherché.
             known = 1;  // Met la variable 'tknown' à 1 si l'ID a été trouvé.
             break;  // Sort de la boucle immédiatement une fois l'ID trouvé.
         }
+        tmp=&knownlist[i];
     }
-    printf("%d\n", known); //Superflu
+    tmp++;
+    if (known==0){
+        if (*tmp==-1){//empeche le debordement
+            for (int i =0;i<(taille-1);i++){
+                knownlist[i]=0;//Réinitialise la knownlist
+            knownlist[0]=id;//Ajoute l'id à la knownlist
+            }
+        printf("Liste reset\n");
+        }
+    else{
+        *tmp=id;//Ajoute l id a la knownlist
+        printf("added\n");
+    }    
+    }
     return known;  // Retourne 1 si l'ID a été trouvé (known = 1), sinon retourne 0 (known = 0).
 }
 
@@ -19,27 +34,19 @@ int main()
     int destinataire =1;
     int id=600;
     int moi=2;
-    int taille =0;
-    int tableau[50]={0};
-    tableau[taille]=700;
-    taille++;
+    int taille =3;
+    int knownlist[3]={0};
+    knownlist[0]=700;//Superflu
+    knownlist[1]=710;//Superflu
+    knownlist[taille-1]=-1;
+    taille++;//Superflu
     if (destinataire==moi){
         printf("Message pour vous\n"); //Superflu
         //Ajoutez fonction pour récup message
     }
-    else if (comparaison(id,tableau,taille)==0){ //Si l'ID est nouveau
-        if (taille<10){
-            tableau[taille]=id;
-            taille++;
-        }
-        else{
-            printf("tableau rempli\n");
-            for (int i =0;i<50;i++){
-                tableau[i]=0;//Réinitialise le tableau
-            }
-        //ajoutez fonction pour sendqueu    
-        }
-            
+    else if (comparaison(id,knownlist,taille)==0){ //Si l'ID est nouveau
+        printf("nice ca mon reuf\n");
+        //ajoutez le send std::queue
     }
     else{
         printf("Ce message est déjà passé\n");//Superflu
@@ -49,8 +56,8 @@ int main()
     
     
     
-    for(int i =0;i<10;i++){
-        printf("%d\n",tableau[i]);
+    for(int i =0;i<(taille-1);i++){
+        printf("%d\n",knownlist[i]);
     }
     return 0;
 }
