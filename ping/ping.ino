6a8e09feff4bcec89915d98fada488665aa18286
Philippe Rochat <prochat@MacBookPro.localdomain>
2025-03-31 23:49:08 +0200

    Cleaned the code and added comments

--- ping/ping.ino
+++ ping/ping.ino
@@ -13,12 +13,13 @@
 
 // Instanciate a new RF95 drive 
 RH_RF95 rf95;
-
+#define TX_POWER 13.  // From 2 to 20dBm emiter power 
+#define FREQUENCY 868 // Europe is 968
 ///// Some globals
 // Data sending
 byte dataoutgoing[RH_RF95_MAX_MESSAGE_LEN];       // A buffer to prepare an outgoing message
 byte lenMsg = 0;                                  // Length of that message
-byte myId[2] = {10,68};                           // My adress
+byte myId[2] = {10,10};                           // My adress (do not leave 10.10)
 byte destId[2] = {0,0};                           // To store destination address
 byte packetId[2];                                 // To store oacket ID
 byte timeStamp[4];                                // To store message time
@@ -32,7 +33,7 @@ byte indatabuf[RH_RF95_MAX_MESSAGE_LEN];          // A buffer to store incoming 
 #define RAR   3      // Reverse adress research
 #define RARA  4      // Reverse adress research answer
 
-/// This defines the msg format
+/// This defines the msg format (see doc)
 #define MSG_POS_TYPE       0
 #define MSG_POS_SENDER_H   1
 #define MSG_POS_SENDER_L   2
@@ -49,13 +50,17 @@ byte indatabuf[RH_RF95_MAX_MESSAGE_LEN];          // A buffer to store incoming 
 // Some config
 #define PING_INTERVAL 10  // Time intervall for sending a PING, rest of time is listening (and answer)
 #define DEBUG_LEVEL   1   // DEBUG to enable some debugging 
+                          // Level 0 or no debug : programm should run silent
+                          // Level 1 : log the main events like sending and receiving
+                          // Level 2 : some part running
+                          // Level 3 :  some details in function like calculus
 
 // Some tools
 #define MAX(a,b) ({ __typeof__ (a) _a = (a); __typeof__ (b) _b = (b); _a > _b ? _a : _b; })
 #define MIN(a,b) ({ __typeof__ (a) _a = (a); __typeof__ (b) _b = (b); _a < _b ? _a : _b; })
 #define LOG_TIME Serial.print((String)"["+(String)millis()+(String)"]")
 
-// This store the current HW time in timestamp 4 bytes array
+// This store the current HW time in timestamp global 4 bytes array
 void setTimeStamp() {
   unsigned long time = millis();
 #if DEBUG_LEVEL >= 3
@@ -70,12 +75,12 @@ void setTimeStamp() {
 #endif  
 }
 
-
+// Get the time from a 4 bytes array and put it down into a returned value
 unsigned long parseTimeStamp(byte buf[4]) {
   unsigned long time = 0;
-  time += buf[0];
-  time <<= 8;
-  time += buf[1];
+  time += buf[0];   // Highest level byte
+  time <<= 8;       // And then shift it to the left
+  time += buf[1];   // Etc ...
   time <<= 8;
   time += buf[2];
   time <<= 8;  
@@ -83,45 +88,53 @@ unsigned long parseTimeStamp(byte buf[4]) {
   return time;
 }
 
-void buildMessage(byte type) {
-  byte broadCast[2] = {0,0};
+// Build a random two bytes packet id
+void setPacketId() {
+  packetId[0] = (byte)random(255);
+  packetId[1] = (byte)random(255);  
+}
+
+////////////////////////////////////////////////////////////////
+// buildMessage build a message stored into dataoutgoing buffer
+// and store len of built message into lenMsg. We have used 
+// Overload technique here to have default params python like style
+void buildMessage(byte type) {  // Specify only type
+  byte broadCast[2] = {0,0};    // Default address is set to broadcast
   buildMessage(type, broadCast);
 }
 
-void buildMessage(byte type, byte dest[2]) {
-  byte packetId[2];
-  packetId[0] = (byte)random(255);
-  packetId[1] = (byte)random(255);
-  buildMessage(type, dest, packetId);
-}
-void buildMessage(byte type, byte dest[2], byte packetId[2]) {
-  buildMessage(type, dest, packetId, NULL);
+void buildMessage(byte type, byte dest[2]) { // ... and specify address
+  setPacketId();        
+  buildMessage(type, dest, NULL); 
 }
 
-void buildMessage(byte type, byte dest[2], byte packetId[2], char *payload) {
+// And specify a payload.
+// This is the real one with all the code
+void buildMessage(byte type, byte dest[2], char *payload) {
+  // Build the header either from params or stored in globals
   dataoutgoing[MSG_POS_TYPE] = type;
   dataoutgoing[MSG_POS_SENDER_H] = myId[0];
   dataoutgoing[MSG_POS_SENDER_L] = myId[1];  
   dataoutgoing[MSG_POS_DEST_H] = destId[0];
   dataoutgoing[MSG_POS_DEST_L] = destId[1];
+  
+  if((type == PING) || (type == RAR)) {    // If this is not an answer
+    setTimeStamp();
+    setPacketId();
+  } // Else it should habve been set  in the request
   dataoutgoing[MSG_POS_PKTID_H] = packetId[0];
   dataoutgoing[MSG_POS_PKTID_L] = packetId[1];
-
-
-  if(type != PONG) {
-    setTimeStamp();
-  }
   dataoutgoing[MSG_POS_TIME_1] = timeStamp[0];
   dataoutgoing[MSG_POS_TIME_2] = timeStamp[1];
   dataoutgoing[MSG_POS_TIME_3] = timeStamp[2];
   dataoutgoing[MSG_POS_TIME_4] = timeStamp[3];
   if(payload == NULL) {
     dataoutgoing[11] = '\0';
-    lenMsg = 12;
+    lenMsg = 12;                            // End of header of 12 bytes
   } else {
     byte len = MIN(strlen(payload), 200);
-    dataoutgoing[11] = 'len';
-    char *str = &dataoutgoing[12];
+    dataoutgoing[11] = 'len';               // End of header
+    char *str = &dataoutgoing[12];          // Enventually add a payload
     strncpy(str, payload, len);
     lenMsg = 12 + len;
   }
@@ -154,87 +167,99 @@ void buildMessage(byte type, byte dest[2], byte packetId[2], char *payload) {
 #endif // Level 1
 }
 
+////////////////////////////////////////////////////////////////
+// parseMessage is to paarse incoming message
 void parseMessage(byte *buffer, uint8_t len) {
-  byte type = buffer[MSG_POS_TYPE];
+  byte type = buffer[MSG_POS_TYPE];       // First extract message type
 #if DEBUG_LEVEL >= 1
   LOG_TIME;
   Serial.print(" RECEIVING -- ");
 #endif
-  if( (buffer[MSG_POS_DEST_H] == myId[0] ) && (buffer[MSG_POS_DEST_L] == myId[1]) ) {
+  if( (buffer[MSG_POS_DEST_H] == myId[0] ) && (buffer[MSG_POS_DEST_L] == myId[1]) ) { // Check if it's for me
 #if DEBUG_LEVEL >= 2
         Serial.println("Thats for me alone!");
 #endif
-  } else if((buffer[MSG_POS_DEST_H] == 0 ) && (buffer[MSG_POS_DEST_L] == 0 ) ) {
+  } else if((buffer[MSG_POS_DEST_H] == 0 ) && (buffer[MSG_POS_DEST_L] == 0 ) ) {      // or for everyone (bcast)
 #if DEBUG_LEVEL >= 2
         Serial.println("Thats for us (bcast)");
 #endif
-  } else {
+  } else {                                                                            // Or not, skip
 #if DEBUG_LEVEL >= 2
     Serial.println("None of my business ...");
 #endif
     return ;
   }
+  // Extract then adresses and packetID
   destId[0] = buffer[MSG_POS_SENDER_H]; // To reply, sender becomes dest.
   destId[1] = buffer[MSG_POS_SENDER_L];
   packetId[0] = buffer[MSG_POS_PKTID_H]; // To reply, packetId is same
   packetId[0] = buffer[MSG_POS_PKTID_L];
-
-  byte answering = 0;
-
+#if DEBUG_LEVEL >= 1
   switch(type) {
     case PING:
-#if DEBUG_LEVEL >= 1
       Serial.print("Type : PING");
-#endif
       break;     
     case PONG:
-#if DEBUG_LEVEL >= 1
       Serial.print("Type : PONG");
-#endif
       break;
   }
-#if DEBUG_LEVEL >= 1
   Serial.print(" // TO : " + (String)buffer[MSG_POS_DEST_H]+"."+(String)buffer[MSG_POS_DEST_L]);
   Serial.print(" // FROM : "+ (String)buffer[MSG_POS_SENDER_H]+"."+(String)buffer[MSG_POS_SENDER_L]);
 #endif
-  unsigned long msgtime = 0;
   switch(type) {
-    case PING:
-      type = PONG;
-      timeStamp[0] = buffer[MSG_POS_TIME_1];
+    case PING:                               // Ping message
+      timeStamp[0] = buffer[MSG_POS_TIME_1]; // Retrive 4 bytes of timestamp
       timeStamp[1] = buffer[MSG_POS_TIME_2];
       timeStamp[2] = buffer[MSG_POS_TIME_3];
       timeStamp[3] = buffer[MSG_POS_TIME_4];
+      packetId[0]  = buffer[MSG_POS_PKTID_H]; // Retrive 2 bytes of pycketId
+      packetId[1]  = buffer[MSG_POS_PKTID_L];
 #if DEBUG_LEVEL >= 1
+      unsigned long msgtime = 0;
       msgtime = parseTimeStamp(&buffer[MSG_POS_TIME_1]);
       Serial.println(" ==> Time in msg : ["+(String)msgtime+"]");
 #endif
-      buildMessage(type, destId, packetId[2]); 
-      answering = 1;
+      buildMessage(PONG, destId); 
       break;     
     case PONG:
-      msgtime = parseTimeStamp(&buffer[MSG_POS_TIME_1]);
-      Serial.print(" ==> Time in msg : ");Serial.print(msgtime);
-      unsigned long delta = millis() - msgtime;
+      msgtime = parseTimeStamp(&buffer[MSG_POS_TIME_1]);  // Get the timestamp, calculate epoch time
+#if DEBUG_LEVEL >= 1
+      Serial.print(" ==> Time in msg : ");Serial.print(msgtime); 
+#endif
+      unsigned long delta = millis() - msgtime;           // And diff to now to get the ping time
+#if DEBUG_LEVEL >= 1
       Serial.print(" DELTA [ms]: ");Serial.println(delta);
-      answering = 0;
+#endif
+      break;
+    case RAR:
+      packetId[0]  = buffer[MSG_POS_PKTID_H]; // Retrive 2 bytes of pycketId
+      packetId[1]  = buffer[MSG_POS_PKTID_L];
+      buildMessage(RARA, destId, myName); 
       break;
   }
-  if(answering) {
+  if((type==PING) || (type=RAR)) {    // We have to send a reply to PING and RAR
 #if DEBUG_LEVEL >= 2
     Serial.print(" --> Sending reply...");
 #endif
     rf95.send(dataoutgoing, sizeof(dataoutgoing));
     rf95.waitPacketSent();
   } 
-#if DEBUG_LEVEL >= 2
+#if DEBUG_LEVEL >= 1
   Serial.println(" ");
 #endif 
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// Main Programm with setup() and loop()
+
 void setup() {
-  Serial.begin(9600);
-  if (!rf95.init())
+  Serial.begin(9600);     // Serial setup
+  Serial.println("Starting ping/pong");
+  rf95.setTxPower(TX_POWER);    
+  rf95.setModemConfig(RH_RF95::Bw125Cr45Sf2048);
+  rf95.setFrequency(FREQUENCY);
+
+  if (!rf95.init())       // Radio init
     Serial.println("init failed");
    // Defaults after init are ... MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
    // Need to change to 868.0Mhz in RH_RF95.cpp 
