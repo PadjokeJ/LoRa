11b500f3057a417edaf6f6db98291fbafabbd4af
lebronanshy <liloi.real@gmail.com>
2025-04-03 13:28:30 +0200

    ss

--- lib/serial/serial.cpp
+++ lib/serial/serial.cpp
@@ -1,26 +1,3 @@
-#include <SPI.h>
-#include <LoRa.h>
-
-const long frequency = 915E6; // Fréquence LoRa (doit correspondre à l'émetteur)
-
-void serialOutput(String message) {
-  Serial.print("Received Data: ");
-  Serial.println(message);
-}
-
-void setup() {
-  Serial.begin(9600);
-  while (!Serial);
-
-  Serial.println("LoRa Receiver Initializing...");
-  
-  if (!LoRa.begin(frequency)) {
-    Serial.println("Starting LoRa failed!");
-    while (1);
-  }
-
-  Serial.println("LoRa Receiver Ready");
-}
 
 void loop() {
   int packetSize = LoRa.parsePacket();
