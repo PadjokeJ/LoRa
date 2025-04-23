9dc57928387d3169be3ca4fff5844f16f671b138
Pad <145267541+PadjokeJ@users.noreply.github.com>
2025-04-23 12:19:06 +0200

    basic decoding and decision making in main loop

--- src/main.ino
+++ src/main.ino
@@ -15,18 +15,66 @@
 #include "../lib/send/send.h"
 #include "../lib/serial/serial.h"
 
+#include "config.h"
+#include <RH_RF95.h>
 
 #define LORA_SS 10     
-#define LORA_FREQ 868  
+#define LORA_FREQ 868
+
+uint8_t messageBytesBuffer[MAX_BYTES_LEN] = {0};
+char messageBuffer[MAX_MESSAGE_LEN] = {0};
+
+bool should_show_message = 0;
 
 Lorainit lora(LORA_SS, LORA_FREQ);
 Receive receiver;
 Send sender;
+
+void resetBuffers(){ 
+    for (int i = 0; i < MAX_BYTES_LEN; i++)
+        messageBytesBuffer[i] = 0;
+    for (int i = 0; i < MAX_MESSAGE_LEN; i++)
+        messageBuffer[i] = 0;
+}
+
+uint8_t decodeAndAnalyseMessage(uint8_t* incomingBytes, char* bufferToModify){
+    struct packet incomingPacket;
+    // decode the message
+    incomingPacket = decode(incomingBytes);
+
+    bool hasSeenThisMessage = comparaison(incomingPacket.identifier, comparison_list, COMPARISON_LIST_SIZE);
+
+    if (hasSeenThisMessage){
+        return MESSAGE_SEEN_CODE;
+    }
+    if(my_address != incomingPacket.destination){
+        return MESSAGE_TO_FORWARD_CODE;
+    }
+    should_show_message = 1;
+    bufferToModify = incomingPacket.message;
+
+    return MESSAGE_TO_ME_CODE;
+    
+}
 
 void setup() {
     lora.init();
 }
 
 void loop() {
-    
+    // <- fetch message into bytes buffer
+
+    //ADD CODE HERE
+
+    // <- decode message, and see what to do with it
+
+    uint8_t result = decodeAndAnalyseMessage(messageBytesBuffer, messageBuffer);
+
+    // <- decide what to do
+    if (result == MESSAGE_SEEN_CODE)
+        ; //do nothing? (include read reciepts, to see if message has gone further)
+    if (result == MESSAGE_TO_FORWARD_CODE)
+        ; //send message again
+    if (result == MESSAGE_TO_ME_CODE)
+        ; //display message on serial
 }
