e0d75acb81eb6ebfebb75410740e358a253ebee3
Pad <145267541+PadjokeJ@users.noreply.github.com>
2025-05-15 12:52:21 +0200

    comment main/test/encode_test.cpp

--- main/test/encode_test.cpp
+++ main/test/encode_test.cpp
@@ -5,26 +5,26 @@
 */
 
 
-#include "../encode.h"
-#include "../packet.h"
-#include "../decode.h"
+#include "../encode.h" // include code for encoding
+#include "../packet.h" // include code for packet structure and management
+#include "../decode.h" // include code for decoding
 
 #include <stdio.h>
 #include <stdint.h>
 
-char message_buffer[200];
+char message_buffer[200]; // create a buffer to decode the message into
 
 int main(){
-    uint8_t packet_type = 0;
-    uint8_t source = 0;
-    uint8_t dest = 1;
+    uint8_t source = 60; // set the user's address
+    uint8_t dest = 53; // set the destination's address
 
-    char message[] = "Hello World!";
+    char message[] = "Hello World!"; // create a string with the message "Hello World!"
 
-    struct packet pack;
+    struct packet pack; // create an empty packet
 
-    pack = encode_message_to_send(source, dest, message);
+    pack = encode_message_to_send(source, dest, message); // call the encode function
 
+    // print out the encoded packet and its proprieties
     printf("ID \t:\t%d\n", pack.identifier);
     printf("Type \t:\t%d\n", pack.type);
     printf("source \t:\t%d\n", pack.source);
@@ -32,16 +32,18 @@ int main(){
     printf("size \t:\t%d\n", pack.size);
     printf("message\t:\t%s\n", pack.message);
 
-    putchar('\n');
+    putchar('\n'); // line break in the console --> air it out
 
-    uint8_t* bytess = pack.encoded_bytes;
-    while(*bytess++){
-        printf("%d, ", *bytess);
+    // debug the encoded bytes
+    uint8_t* encode_bytes = pack.encoded_bytes; 
+    while(*encode_bytes++){
+        printf("%d, ", *encode_bytes);
     }
-    putchar('\n');
+    putchar('\n'); // line break in the console --> air it out
 
-    struct packet pack2 = decode(pack.encoded_bytes, message_buffer);
+    struct packet pack2 = decode(pack.encoded_bytes, message_buffer); // call the decoded function, set it to a new packet structure
 
+    // print out the decoded packet and its proprieties
     printf("ID \t:\t%d\n", pack2.identifier);
     printf("Type \t:\t%d\n", pack2.type);
     printf("source \t:\t%d\n", pack2.source);
@@ -49,7 +51,9 @@ int main(){
     printf("size \t:\t%d\n", pack2.size);
     printf("message\t:\t%s\n", pack2.message);
 
-    printf("buffer \t:\t%s\n", message_buffer);
+    printf("buffer \t:\t%s\n", message_buffer); // print out the message we decoded into the buffer
+
+    // if the encode and decode code is correct, we should have the same proprieties and message when we print them out
 
     return 0;
 }
